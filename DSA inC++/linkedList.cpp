481a32ef8490ad5009c73ab781cc8b1a7dcf213f
Swayy03 <sweetmuffin03@gmail.com>
2023-07-07 03:28:44 +0300

    SinglyLinkedList
    A short dive into the implementation of singly linked lists. Created 2 classes Node and SinglyLinkedList and implemented the basic functions of a linked list to it, i.e Traverse, Append, Prepend and almost done with the insertAfterNode function

--- DSA inC++/linkedList.cpp
+++ DSA inC++/linkedList.cpp
@@ -3,16 +3,41 @@
 
 using namespace std;
 
-int main(){
-	int luckyNumbers[5];
-	for(int i = 0; i<5;i++){
-		cout<<"index "<<i<<": ";
-		cin>>luckyNumbers[i];
-	}	
-	for(int i = 0; i<=5;i++){
-		cout<<*(luckyNumbers +i)<<" ";
+class Node{
+	public:
+		int key;
+		int data;
+		Node* next;
+		
+		Node(){
+			key = 0;
+			data = 0;
+			next  = NULL;
+		}
+		Node(int k,int d){
+			key = k;
+			data = d;
+			next = NULL;
+		}
+};
+
+class SinglyLinkedList{
+	public:
+		Node* head;
 	
-	}	
-	system("pause>0");
+};
+
+int main(){
+	
+	
+	Node n1(1,10);
+	Node n2(2,20);
+
+    int n=5;
+    int *p = &n;
+    cout<< &n <<endl;
+	
+	
 	return 0;
+	
 }
